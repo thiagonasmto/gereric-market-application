c2a7a854f45f7742e0f2c1b97ff6dceb59e11d7c
thiagonasmto <thiagonasmento20@gmail.com>
2025-07-30 23:01:50 -0300

    fix: fechamento da rota para criação de adms

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -20,8 +20,7 @@ func UserRoutes(router *gin.Engine) {
 
 	admGroup := router.Group("/adms")
 	{
-		admGroup.POST("/", controllers.CreateAdm)
-		// admGroup.POST("/", middlewares.AuthMiddleware(), middlewares.RequireRoleMiddleware("admin"), controllers.CreateAdm)
+		admGroup.POST("/", middlewares.AuthMiddleware(), middlewares.RequireRoleMiddleware("admin"), controllers.CreateAdm)
 		admGroup.GET("/", middlewares.AuthMiddleware(), middlewares.RequireRoleMiddleware("admin"), controllers.GetAdms)
 		admGroup.GET("/:id", middlewares.AuthMiddleware(), middlewares.RequireRoleMiddleware("admin"), controllers.GetAdm)
 		admGroup.PUT("/:id", middlewares.AuthMiddleware(), middlewares.RequireRoleMiddleware("admin"), controllers.UpdateAdm)
